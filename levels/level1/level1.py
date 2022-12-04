cf5bed2aa97048117a55b85ced8838d941305322
Louis LaCroix <lacro038@gmail.com>
2022-12-04 12:32:15 -0800

    Day 2 part 1 complete.

--- levels/level1/level1.py
+++ levels/level1/level1.py
@@ -3,28 +3,23 @@ Process each line, sum each until you reach a blank line. Then compare the sum
 to the max and keep going. """
 
 
-
 def single_elf():
     with open('levels/level1/level1data.txt') as input:
         max_calories = 0
         line = input.readline()
         elf_calories = 0
 
-        while line != '': # The EOF char is an empty string
+        while line != '':  # The EOF char is an empty string
             if line == "\n":
-                #print(f"Current elf_calories is {elf_calories}. Current max_calories is {max_calories}. Updating max_calories if necessary.")
                 max_calories = max(max_calories, elf_calories)
                 elf_calories = 0
                 line = input.readline()
 
             else:
-                #print(line, end='')
                 elf_calories += int(line)
                 line = input.readline()
-        
-        #print(f"Current elf_calories is {elf_calories}. Current max_calories is {max_calories}. Updating max_calories if necessary.")
+
         max_calories = max(max_calories, elf_calories)
-        
 
     print(f"Max calories brought by any elf was: {max_calories}!")
 
@@ -36,11 +31,9 @@ def three_elves():
         elf_calories = 0
         calories_list = []
 
-        while line != '': # The EOF char is an empty string
+        while line != '':  # The EOF char is an empty string
             if line == "\n":
-                #print(f"Current elf_calories is {elf_calories}. Current max_calories is {max_calories}. Updating max_calories if necessary.")
                 calories_list.append(elf_calories)
-                #max_calories = max(max_calories, elf_calories)
                 elf_calories = 0
                 line = input.readline()
 
@@ -48,15 +41,13 @@ def three_elves():
                 #print(line, end='')
                 elf_calories += int(line)
                 line = input.readline()
-        
+
         calories_list.sort(reverse=True)
         total_calories = sum(calories_list[:3])
-        #print(f"Current elf_calories is {elf_calories}. Current max_calories is {max_calories}. Updating max_calories if necessary.")
-        #max_calories = max(max_calories, elf_calories)
-        
 
-    print(f"Total calories brought by the top three elves was: {total_calories}!")
+    print(
+        f"Total calories brought by the top three elves was: {total_calories}!")
+
 
 single_elf()
 three_elves()
-
---
+++ levels/level2/level2.py
@@ -0,0 +1,63 @@
+""" Solution ideas and notes:
+
+First column: 
+A = Rock
+B = Paper
+c = Scissors
+
+Second column:
+X = Rock
+Y = Paper
+Z = Scissors
+
+Open the file, read each line, first calculate the score for the shape you selected, (1 for Rock, 2 for Paper, and 3 for Scissors)  
+
+Rock paper scissors logic: If you win you get 6 points, draw = 3, loss = 0
+"""
+
+player1_dict = {
+    "A": ["Rock", 1],
+    "B": ["Paper", 2],
+    "C": ["Scissors", 3]
+}
+
+
+player2_dict = {
+    "X": ["Rock", 1],
+    "Y": ["Paper", 2],
+    "Z": ["Scissors", 3]
+}
+
+def rock_paper_scissors(file_name):
+    with open(file=file_name) as file:
+        total_score = 0
+        line = file.readline()
+        while line != '':
+            player1_choice = line[0]
+            player2_choice = line[2]
+            print(f"Player 1 plays {player1_dict[player1_choice][0]}, \
+Player 2 plays {player2_dict[player2_choice][0]}.")
+            if player1_dict[player1_choice][0] == player2_dict[player2_choice][0]:
+                print(f"The round was a draw, each player gets 3 points.")
+                total_score += 3
+            elif player1_dict[player1_choice][0] == "Rock" and player2_dict[player2_choice][0] == "Paper":
+                print(f"Player 2 wins.")
+                total_score += 6
+            elif player1_dict[player1_choice][0] == "Rock" and player2_dict[player2_choice][0] == "Scissors":
+                print(f"Player 1 wins.")
+            elif player1_dict[player1_choice][0] == "Paper" and player2_dict[player2_choice][0] == "Scissors":
+                print(f"Player 2 wins.")
+                total_score += 6
+            elif player1_dict[player1_choice][0] == "Paper" and player2_dict[player2_choice][0] == "Rock":
+                print(f"Player 1 wins.")
+            elif player1_dict[player1_choice][0] == "Scissors" and player2_dict[player2_choice][0] == "Rock":
+                print(f"Player 2 wins.")
+                total_score += 6
+            elif player1_dict[player1_choice][0] == "Scissors" and player2_dict[player2_choice][0] == "Paper":
+                print(f"Player 1 wins.")
+            total_score += player2_dict[player2_choice][1]
+            print(f"Current score is: {total_score}")
+            line = file.readline()
+
+
+rock_paper_scissors('levels/level2/level2data.txt')
