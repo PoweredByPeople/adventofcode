777ef0a948725d532b448b430992c1f985bc8fee
Louis LaCroix <lacro038@gmail.com>
2022-12-08 14:48:55 -0800

    Day 6 part 1.

---
+++ levels/level6/level6.py
@@ -0,0 +1,19 @@
+"""
+Day 6!
+"""
+
+def start_of_packet(filename):
+    with open(filename, encoding="utf-8") as datastream_buffer:
+        data = datastream_buffer.read()
+        l, r = 0, 4
+
+        while r < len(data):
+            chars = data[l:r]
+            if len(chars) > len(set(chars)):
+                l += 1
+                r += 1
+            else:
+                print(r)
+                break
+
+start_of_packet("levels/level6/level6data.txt")
